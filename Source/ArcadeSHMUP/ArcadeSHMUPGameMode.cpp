7515d4e01399f90870ba52a4cd68ee94cc95be1e
Vlad <Kulini4vlad@ukr.net>
2018-10-25 09:55:23 +0200

    Initial Commit

---
+++ Source/ArcadeSHMUP/ArcadeSHMUPGameMode.cpp
@@ -0,0 +1,11 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "ArcadeSHMUPGameMode.h"
+#include "ArcadeSHMUPPawn.h"
+
+AArcadeSHMUPGameMode::AArcadeSHMUPGameMode()
+{
+	// set default pawn class to our character class
+	DefaultPawnClass = AArcadeSHMUPPawn::StaticClass();
+}
+
